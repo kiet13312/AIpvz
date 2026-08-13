52b2036ba65a50d9fe99c3979a454d5b82d623f9
kiet13312 <memaybeo9007@gmail.com>
2026-08-13 20:54:42 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/alpvz/gardendefense/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alpvz.gardendefense;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Garden Defense");
+        text.setTextSize(24);
+        text.setPadding(40, 40, 40, 40);
+
+        setContentView(text);
+    }
+                        }
