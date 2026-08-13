8436f75e1579c1c0f7717c2fc0eb4e421669e432
kiet13312 <memaybeo9007@gmail.com>
2026-08-13 20:42:29 +0700

    Delete MainActivity.java

--- MainActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.pvzlike;
-import android.app.Activity;
-import android.os.Bundle;
-public class MainActivity extends Activity { @Override public void onCreate(Bundle b){super.onCreate(b);} }
