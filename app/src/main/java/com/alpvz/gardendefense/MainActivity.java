3bde2c17366f4e0d2d77d8df05c55ed77f9eff42
kiet13312 <memaybeo9007@gmail.com>
2026-08-14 10:02:01 +0700

    Update MainActivity.java

--- app/src/main/java/com/alpvz/gardendefense/MainActivity.java
+++ app/src/main/java/com/alpvz/gardendefense/MainActivity.java
@@ -2,7 +2,10 @@ package com.alpvz.gardendefense;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +13,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView text = new TextView(this);
-        text.setText("Garden Defense");
-        text.setTextSize(24);
-        text.setPadding(40, 40, 40, 40);
+        LinearLayout battle = new LinearLayout(this);
+        battle.setOrientation(LinearLayout.HORIZONTAL);
+        battle.setGravity(Gravity.CENTER);
+        battle.setBackgroundColor(Color.rgb(30, 100, 30));
+        battle.setPadding(20, 20, 20, 20);
 
-        setContentView(text);
+        ImageView sunflower = new ImageView(this);
+        sunflower.setImageResource(R.drawable.sunflower);
+        sunflower.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView peashooter = new ImageView(this);
+        peashooter.setImageResource(R.drawable.peashooter);
+        peashooter.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView wallnut = new ImageView(this);
+        wallnut.setImageResource(R.drawable.wallnut);
+        wallnut.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        int size = 300;
+
+        battle.addView(sunflower, new LinearLayout.LayoutParams(size, size));
+        battle.addView(peashooter, new LinearLayout.LayoutParams(size, size));
+        battle.addView(wallnut, new LinearLayout.LayoutParams(size, size));
+
+        setContentView(battle);
     }
-                        }
+}
